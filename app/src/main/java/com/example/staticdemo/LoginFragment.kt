c5de7f97338ec9ac609487b7fda9266022e19e51
maryanaantonyuk <amaryana@gmail.com>
2019-09-30 23:44:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/staticdemo/LoginFragment.kt
@@ -0,0 +1,41 @@
+package com.example.staticdemo
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [LoginFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.findViewById<Button>(R.id.executeLoginButton).setOnClickListener { v ->
+            v.findNavController().navigate(R.id.action_loginFragment_to_quizFragment)
+        }
+        return view
+
+    }
+
+}
---
+++ app/src/main/java/com/example/staticdemo/ui/home/HomeFragment.kt
@@ -0,0 +1,37 @@
+package com.example.staticdemo.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import com.example.staticdemo.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(this, Observer {
+//            textView.text = it
+//        })
+        val loginButton: Button = root.findViewById(R.id.loginButton)
+        loginButton.setOnClickListener { v ->
+            v.findNavController().navigate(R.id.action_navigation_home_to_loginFragment)
+        }
+        return root
+    }
+}
